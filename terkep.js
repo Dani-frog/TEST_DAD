f3cdb7df91e48ba2dc9a8f1f0f5c44695c201300
bUMmrR <agocsdaniel5454@gmail.com>
2024-04-04 10:11:19 +0200

    a canvas kezdete

---
+++ terkep.js
@@ -0,0 +1,6 @@
+const terkep = document.getElementById("minta");
+const ctxTerkep = terkep.getContext("2d");
+const terkepKep = document.createElement("img");
+terkep.height = 450;
+terkep.width = 450;
+terkep.src="Kepek/alaptorta.png";
