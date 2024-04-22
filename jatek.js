ad6bd25c4398f7d7659e9d22dd9ed3ed62347c1f
bUMmrR <agocsdaniel5454@gmail.com>
2024-04-22 12:16:39 +0200

    tavolsag kiirasa consolba

--- jatek.js
+++ jatek.js
@@ -1,52 +1,41 @@
 var szunet = true;
 var x;
 var y;
+var jelenlegiKerdes = -1;
+var joKerdesek;
+var kerdesDiv;
 
 
 async function nehezseg(btn) {
-
     szunet = false;
+    
     await kerdesektombfeltolt();
     shuffle(kerdesektomb)
-    console.log(kerdesektomb);
-    console.log("itt vok");
+
     const gombok = document.getElementsByClassName("gombok")
     for (let index = 0; index < gombok.length; index++) {
         gombok[index].innerHTML = "";
     }
-    if (btn.value == "Könnyű") {
-        console.log(btn.value);
-        var mondatkerdes = kerdesektomb.find(obj => obj.nehezseg == "Könnyű");
-        for (let index = 0; index < gombok.length; index++) 
-        {
-            gombok[index].innerHTML = mondatkerdes.kerdes; 
-        }
-    }
-    
-    else if (btn.value == "Közepes") {
-        console.log(btn.value);
-        var mondatkerdes  = kerdesektomb.find(obj => obj.nehezseg == "Közepes");
-        for (let index = 0; index < gombok.length; index++)
-        {
-            gombok[index].innerHTML = mondatkerdes.kerdes; 
-        }
-    }
-    else if (btn.value == "Nehéz") {
-        console.log(btn.value);
-        var mondatkerdes  = kerdesektomb.find(obj => obj.nehezseg == "Nehéz");
-        for (let index = 0; index < gombok.length; index++) 
-        { 
-            gombok[index].innerHTML = mondatkerdes.kerdes; 
-        }
-    }
-    else{
-        console.log("NEMJÓ");
-    }
+
+    joKerdesek  = kerdesektomb.filter(obj => obj.nehezseg == btn.value);
+
+    kerdesDiv = document.createElement("div");
+    gombok[0].appendChild(kerdesDiv);
+
+    kerdesKiiras();
+
     let gomb = document.createElement("input")
     gomb.type = "button";
-    gomb.value = "Tovább"
-    document.getElementsByClassName("gombok")[0].appendChild(gomb);
+    gomb.value = "Tovább";
+    gomb.setAttribute("onclick","pontozas()")
+
+    gombok[0].appendChild(gomb);
 } 
+
+function kerdesKiiras(){
+    jelenlegiKerdes++;
+    kerdesDiv.innerHTML = joKerdesek[jelenlegiKerdes].kerdes;
+}
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -59,4 +48,18 @@ function shuffle(array) {
 function terkepreKatt(xKord,yKord){
     x = xKord;
     y = yKord;
+}
+
+function pontozas(){
+    let helyesx = joKerdesek[jelenlegiKerdes].xy.split(";")[0]
+    let helyesy = joKerdesek[jelenlegiKerdes].xy.split(";")[1]
+
+    console.log(helyesx*2 - x,helyesy*2 -y)
+
+    const deltaX = helyesx - x;
+    const deltaY = helyesy - y;
+
+    var temp = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    console.log(temp1)
+
 }
