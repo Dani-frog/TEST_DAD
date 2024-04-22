e78bf2da43cea37f2546e4f07f63b64fed6a2102
bUMmrR <agocsdaniel5454@gmail.com>
2024-04-22 11:38:41 +0200

    a normal mod müködésének kezdete

---
+++ jatek.js
@@ -0,0 +1,62 @@
+var szunet = true;
+var x;
+var y;
+
+
+async function nehezseg(btn) {
+
+    szunet = false;
+    await kerdesektombfeltolt();
+    shuffle(kerdesektomb)
+    console.log(kerdesektomb);
+    console.log("itt vok");
+    const gombok = document.getElementsByClassName("gombok")
+    for (let index = 0; index < gombok.length; index++) {
+        gombok[index].innerHTML = "";
+    }
+    if (btn.value == "Könnyű") {
+        console.log(btn.value);
+        var mondatkerdes = kerdesektomb.find(obj => obj.nehezseg == "Könnyű");
+        for (let index = 0; index < gombok.length; index++) 
+        {
+            gombok[index].innerHTML = mondatkerdes.kerdes; 
+        }
+    }
+    
+    else if (btn.value == "Közepes") {
+        console.log(btn.value);
+        var mondatkerdes  = kerdesektomb.find(obj => obj.nehezseg == "Közepes");
+        for (let index = 0; index < gombok.length; index++)
+        {
+            gombok[index].innerHTML = mondatkerdes.kerdes; 
+        }
+    }
+    else if (btn.value == "Nehéz") {
+        console.log(btn.value);
+        var mondatkerdes  = kerdesektomb.find(obj => obj.nehezseg == "Nehéz");
+        for (let index = 0; index < gombok.length; index++) 
+        { 
+            gombok[index].innerHTML = mondatkerdes.kerdes; 
+        }
+    }
+    else{
+        console.log("NEMJÓ");
+    }
+    let gomb = document.createElement("input")
+    gomb.type = "button";
+    gomb.value = "Tovább"
+    document.getElementsByClassName("gombok")[0].appendChild(gomb);
+} 
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+function terkepreKatt(xKord,yKord){
+    x = xKord;
+    y = yKord;
+}
